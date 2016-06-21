4aae85be715db65364f2d5b63d748d9e28344ecf
Brian Schrader <brian@brianschrader.com>
2016-06-21 12:21:09 -0700

    Adds static dir

--- gene_pc_api/settings.py
+++ gene_pc_api/settings.py
@@ -167,6 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.environ.get('STATIC_ROOT', None)
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
