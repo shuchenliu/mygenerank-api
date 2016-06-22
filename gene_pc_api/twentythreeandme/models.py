c8c3169c4372862cda670f1a273cdcae96bd8bf7
Bhuvan Molparia <bhuvan.molparia@gmail.com>
2016-06-22 16:02:41 -0700

    changes the tasks and api-client to work with the new OAUTH system

--- gene_pc_api/twentythreeandme/models.py
+++ gene_pc_api/twentythreeandme/models.py
@@ -31,7 +31,6 @@ class User(models.Model):
 
         return uobj
 
-
 class Profile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE,
--- gene_pc_api/twentythreeandme/tasks.py
+++ gene_pc_api/twentythreeandme/tasks.py
@@ -1,28 +1,46 @@
 from celery import shared_task
 
-from .models import Genotype, Profile, User
-from gene_pc_api.gene_pc_api.models import User as AppUser
-from .api_client import *
+from .models  import User, Profile, Genotype
+from .api_client import get_user_info, get_genotype_data
+import requests
+from  django.core.exceptions import ObjectDoesNotExist
 
-#import logging
+@shared_task
+def twentythreeandme_delayed_import_task(token, userid):
+    """ Given a token and a user info JSON object this will
+    create a API user and  23andMe User and return the API user.
+    It will also spawn off processes to create Profile objects and
+    import genotype data. """
 
-def twentythreeandme_import_task(user_info, token):
+    try:
+        user_info = get_user_info(token)
+        user_id = user_info['id']
+        try:
+            """ If a user_id exists already then just display that user """
+            existing_user_ttm = User.objects.get(user_id__exact=user_id)
+
+        except ObjectDoesNotExist:
+            """ Create a new user and start the import tasks
+            if the user doesn't exist """
+            twentythreeandme_import_task.delay(user_info, token, userid)
+
+    except requests.exceptions.Timeout:
+        twentythreeandme_delayed_import_task.delay(token,userid, countdown = 2)
+
+@shared_task
+def twentythreeandme_import_task(user_info, token, userid):
     """ Given a token and a user info JSON object this will
     create a API user and  23andMe User and return the API user.
     It will also spawn off processes to create Profile objects and
     import genotype data. """
 
     ttm_uobj = User.from_json(user_info, token)
-    gpc_uobj = AppUser.new_user(ttm_uobj.email)
-    ttm_uobj.apiuserid = gpc_uobj.id
+    ttm_uobj.apiuserid = userid
     ttm_uobj.save()
-    gpc_uobj.save()
 
     for profile_info in user_info['profiles']:
         twentythreeandme_profile_import_task.delay(ttm_uobj, profile_info,
-                                                     token)
-    return gpc_uobj
-
+                                                             token)
 
 @shared_task
 def twentythreeandme_profile_import_task(user, profile_info, token):
@@ -35,8 +53,12 @@ def twentythreeandme_profile_import_task(user, profile_info, token):
 
 @shared_task
 def twentythreeandme_genotype_import_task(profile,token):
-    genotype_data = get_genotype_data(token,profile)
-    genotype = Genotype.from_json(genotype_data,profile)
+    try:
+        genotype_data = get_genotype_data(token,profile)
+        genotype = Genotype.from_json(genotype_data,profile)
+        genotype.save()
+    except requests.exceptions.Timeout:
+        twentythreeandme_genotype_import_task.delay(profile,token, countdown=2)
 
 @shared_task
 def convert_genotype_task(genotype):
