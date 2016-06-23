c72d93de86c99e8a8689255b115bce170e4ad497
Brian Schrader <brian@brianschrader.com>
2016-06-22 18:18:06 -0700

    Adds notification dispatching.

--- gene_pc_api/gene_pc_api/signals.py
+++ gene_pc_api/gene_pc_api/signals.py
@@ -2,7 +2,9 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.core.mail import send_mail
 
-from .models import User, Activity, ActivityStatus
+from push_notifications.models import APNSDevice
+
+from .models import User, Activity, ActivityStatus, RiskScore
 from .tasks import send_registration_email_to_user
 
 
@@ -10,9 +12,7 @@ from .tasks import send_registration_email_to_user
 def create_related_models_for_user(sender, instance, created, **kwargs):
     """ Whenever a user is created, also create any related models. """
     if created:
-        for activity in Activity.objects.all():
-            status = ActivityStatus(user=instance, activity=activity)
-            status.save()
+        create_statuses_for_new_user.delay(instance)
 
 
 @receiver(post_save, sender=User)
@@ -27,8 +27,23 @@ def create_status_for_old_users(sender, instance, created, **kwargs):
     """ Whenever a new Activity is created, add status objects to
     all existing users.
     """
-    if not created:
-        return
-    for user in User.objects.all():
-        status = ActivityStatus(user=user, activity=instance)
-        status.save()
+    if created:
+        create_statuses_for_existing_users.delay(instance)
+
+
+@receiver(post_save, sender=RiskScore)
+def send_nofitication_for_new_risk_score(sender, instance, created, **kwargs):
+    """ Whenever a new risk score is created for a given user, send them
+    a notification to let them know.
+    """
+    if created:
+        send_risk_score_notification.delay(instance)
+
+
+@receiver(post_save, sender=Activity)
+def send_notification_for_new_activity(sender, instance, created, **kwargs):
+    """ Whenever a new Activity is created, send a notification to
+    all existing users.
+    """
+    if created:
+        send_activity_notification.delay(instance)
--- gene_pc_api/gene_pc_api/tasks.py
+++ gene_pc_api/gene_pc_api/tasks.py
@@ -12,3 +12,30 @@ def send_registration_email_to_user(user):
     html = render_to_string('registration_email.html', {'url': url})
     send_mail(settings.REGISTER_EMAIL_SUBJECT, '', settings.EMAIL_HOST_USER,
         [user.email], fail_silently=False, html_message=html)
+
+
+@shared_task
+def send_risk_score_notification(risk_score):
+    device = APNSDevice.objects.get(user__id=risk_score.user.id)
+    device.send("Your risk score for {condition} is available.".format(
+        condition=risk_score.condition.name))
+
+
+@shared_task
+def send_activity_notification(activity):
+    devices = GCMDevice.objects.filter(active=True)
+    devices.send_message('A new activity is ready for you!')
+
+
+@shared_task
+def create_statuses_for_existing_users(activity):
+    for user in User.objects.all():
+        status = ActivityStatus(user=user, activity=activity)
+        status.save()
+
+
+@shared_task
+def create_statuses_for_new_user(user):
+    for activity in Activity.objects.all():
+        status = ActivityStatus(user=user, activity=activity)
+        status.save()
