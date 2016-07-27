a22594e323da904e21c8284cc2fab935801b62ae
Brian Schrader <brian@brianschrader.com>
2016-07-27 12:03:50 -0700

    Adds correct celery config

--- generank/__init__.py
+++ generank/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import
+from . import celery
 
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-from .celery import app as celery_app  # noqa
+celery  # bootstrap Celery
