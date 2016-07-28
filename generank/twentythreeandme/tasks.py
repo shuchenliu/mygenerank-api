5f61aea35c4a4b5aa6ab029eb52d83c15071d823
Bhuvan Molparia <bhuvan.molparia@gmail.com>
2016-07-28 13:59:03 -0700

    updates the docs for the 23 and me tasks

--- generank/twentythreeandme/tasks.py
+++ generank/twentythreeandme/tasks.py
@@ -7,9 +7,9 @@ from .api_client import get_user_info, get_genotype_data
 
 @shared_task
 def twentythreeandme_delayed_import_task(token, api_userid, profileid):
-    """ Given a token and a api_user and a 23andMe profile_id,
+    ''' Given a token and a api_user and a 23andMe profile_id,
     it fetches user data for that profile from 23andMe and starts
-    the task to create a 23andMe User/Profile. """
+    the task to create a 23andMe User/Profile. '''
 
     try:
         user_info = get_user_info(token)
@@ -23,10 +23,9 @@ def twentythreeandme_delayed_import_task(token, api_userid, profileid):
 
 @shared_task
 def twentythreeandme_import_task(user_info, token, api_userid, profileid):
-    """ Given a token and a user info JSON object this will
-    create a API user and  23andMe User and return the API user.
-    It will also spawn off processes to create Profile objects and
-    import genotype data. """
+    ''' Given a token and a user info JSON object this will create a 23andMe
+    User. It will also create a Profile object and spawn a job to import the
+    genotype data. '''
 
     # Create a ttm user
     ttm_uobj = User.from_json(user_info, token)
@@ -42,6 +41,10 @@ def twentythreeandme_import_task(user_info, token, api_userid, profileid):
 
 @shared_task
 def twentythreeandme_genotype_import_task(profile,token):
+    ''' Given a profile object and a bearer token, this function will download
+    the raw genotype data from 23andme and save it in a genotype object and
+    spawns a job to convert the raw file into the VCF format. '''
+    
     try:
         genotype_data = get_genotype_data(token,profile)
         genotype = Genotype.from_json(genotype_data,profile)
