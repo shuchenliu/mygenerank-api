29bbc42d55999f6a510795b98440a02f83848f86
Brian Schrader <brian@brianschrader.com>
2016-09-07 14:02:04 -0700

    Cleans up tasks

--- generank/api/tasks.py
+++ generank/api/tasks.py
@@ -18,8 +18,14 @@ from .models import User, Activity, ActivityStatus, RiskScore
 logger = get_task_logger(__name__)
 
 
+# Email Tasks
+
+
 @shared_task
 def send_registration_email_to_user(registration_url, registration_code, user_email):
+    """ Given a user and registration information, send a registration
+    email to that user.
+    """
     logger.debug('tasks.send_registration_email_to_user')
     html = render_to_string('registration_email.html', {
         'url': registration_url,
@@ -28,6 +34,9 @@ def send_registration_email_to_user(registration_url, registration_code, user_em
     logger.info('Sending email to new user: %s' % user_email)
     send_mail(settings.REGISTER_EMAIL_SUBJECT, '', settings.EMAIL_HOST_USER,
         [user_email], fail_silently=False, html_message=html)
+
+
+# Notification Tasks
 
 
 @shared_task
@@ -53,6 +62,9 @@ def send_activity_notification(activity_id):
     logger.info('New mass activity notification sent to all users.')
 
 
+# Create New Model Tasks
+
+
 @shared_task
 def create_statuses_for_existing_users(activity_id):
     logger.debug('tasks.create_statuses_for_existing_users')
@@ -69,7 +81,7 @@ def create_statuses_for_new_user(user_id):
     user = User.objects.get(id=user_id)
     activities = [
         Activity.objects.get(study_task_identifier=settings.PHENOTYPE_SURVEY_ID),
-        Activity.objects.get(study_task_identifier=GENOTYPE_AUTH_SURVEY_ID)
+        Activity.objects.get(study_task_identifier=settings.GENOTYPE_AUTH_SURVEY_ID)
     ]
 
     for activity in activities:
@@ -77,16 +89,19 @@ def create_statuses_for_new_user(user_id):
         status.save()
     logger.info('New statuses created for new user: %s' % user_id)
 
+
 @shared_task
 def send_post_cad_survey_to_users(user_id):
     """ On completion of the Risk Score calculation add the Post CAD survey
-    status for the user. """
+    status for the user.
+    """
     logger.debug('tasks.send_post_cad_survey_to_users')
     user = User.objects.get(id=user_id)
-    activity = Activity.objects.get(study_task_identifier=POST_CAD_RESULTS_SURVEY_ID)
+    activity = Activity.objects.get(study_task_identifier=settings.POST_CAD_RESULTS_SURVEY_ID)
     status = ActivityStatus(user=user, activity=activity)
     status.save()
     logger.info('Post CAD survey status created for user: %s' % user_id)
+
 
 @shared_task
 def send_followup_survey_to_users():
