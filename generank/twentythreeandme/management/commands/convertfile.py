9a150ac977acc717c868b5a1b11becf692795c29
Brian Schrader <brian@brianschrader.com>
2016-10-03 14:01:54 -0700

    Changes command params since email might not be unique

--- generank/twentythreeandme/management/commands/convertfile.py
+++ generank/twentythreeandme/management/commands/convertfile.py
@@ -9,33 +9,33 @@ class Command(BaseCommand):
     help = 'Begins the process of converting a user\'s data for CAD calculations.'
 
     def add_arguments(self, parser):
-        parser.add_argument('email', type=str)
+        parser.add_argument('user_id', type=str)
         parser.add_argument('profile_id', type=str)
 
     def handle(self, *args, **options):
-        email = options['email']
+        user_id = options['user_id']
         profile_id = options['profile_id']
 
         try:
-            user = User.objects.get(email=email)
+            user = User.objects.get(user_id=user_id)
         except ObjectDoesNotExist:
-            self.stdout.write(self.style.FAILURE('No such user.'))
+            self.stdout.write(self.style.ERROR('No such user.'))
             return
 
         try:
             profile = Profile.objects.get(user=user, profile_id=profile_id)
         except ObjectDoesNotExist:
-            self.stdout.write(self.style.FAILURE('No such profile for user.'))
+            self.stdout.write(self.style.ERROR('No such profile for user.'))
             return
 
         try:
             genotype = profile.genotype
         except ObjectDoesNotExist:
-            self.stdout.write(self.style.FAILURE('User has no genotypes.'))
+            self.stdout.write(self.style.ERROR('User has no genotypes.'))
             return
 
         if not genotype.genotype_file:
-            self.stdout.write(self.style.FAILURE('File does not exist.'))
+            self.stdout.write(self.style.ERROR('File does not exist.'))
             return
 
         tasks.convert_genotype_task.delay(genotype)
