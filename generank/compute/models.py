2e3aef0dc72aed248b55d4d4d26b8b71c8d32175
Brian Schrader <brian@brianschrader.com>
2017-03-09 17:53:46 -0800

    Adds new task reporting (#68)

--- generank/compute/models.py
+++ generank/compute/models.py
@@ -1,3 +1,21 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from generank.api import models as api
+
+
+class TaskStatus(models.Model):
+    """ A record of the status of a task at a given point in time. """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(api.User, related_name='task_statuses',
+        on_delete=models.CASCADE)
+    identifier = models.CharField(max_length=100, blank=True)
+    status = models.CharField(max_length=100, blank=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    def __str__(self):
+        return '<Compute TaskStatus: %s %s -- %s' % (self.identifier,
+            self.user.username, self.status)
