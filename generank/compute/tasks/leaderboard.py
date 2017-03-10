9d176ca189fc76353e1f5000c3d6612261b6778c
Brian Schrader <brian@brianschrader.com>
2017-03-10 10:41:08 -0800

    Adds new reporting to leaderboard

--- generank/compute/tasks/leaderboard.py
+++ generank/compute/tasks/leaderboard.py
@@ -12,25 +12,27 @@ def get_leaderboard_status(user_id):
     """ For a given user extract the summary of their activity for a given
     week and rank it against the others in their risk category.
     """
-    walking_activity = models.HealthSample.objects.filter(
-        user__id=user_id,
-        start_date__gte=datetime.utcnow() - timedelta(days=7),
-        identifier="HKWalkRun"
-    )
-    total_step_count = sum([sample.value for sample in walking_activity])
-    rank = len(ActivityScore.objects.filter(
-        value__gt=total_step_count,
-        created_on__gte=datetime.utcnow() - timedelta(days=7),
-    ))
+    with record('tasks.leaderboard.get_leaderboard_status', user_id):
+        walking_activity = models.HealthSample.objects.filter(
+            user__id=user_id,
+            start_date__gte=datetime.utcnow() - timedelta(days=7),
+            identifier="HKWalkRun"
+        )
+        total_step_count = sum([sample.value for sample in walking_activity])
+        rank = len(ActivityScore.objects.filter(
+            value__gt=total_step_count,
+            created_on__gte=datetime.utcnow() - timedelta(days=7),
+        ))
 
-    previous = ActivityScore.objects.all().order_by('-created_on')[0]
-    delta = previous.rank - rank
+        previous = ActivityScore.objects.all().order_by('-created_on')[0]
+        delta = previous.rank - rank
 
-    return total_step_count, rank, delta
+        return total_step_count, rank, delta
 
 
 @shared_task
 def store_results(user_id, value, rank, delta)
-    user = User.objects.get(id=user_id)
-    score = models.ActivityScore(user=user, value=value, rank=rank, delta=delta)
-    score.save()
+    with record('tasks.leaderboard.store_results', user_id):
+        user = User.objects.get(id=user_id)
+        score = models.ActivityScore(user=user, value=value, rank=rank, delta=delta)
+        score.save()
