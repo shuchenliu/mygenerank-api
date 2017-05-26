18fa3d9a3faf2185abe8f30561ad01a769ee2119
Brian Schrader <brian@brianschrader.com>
2017-05-26 10:23:43 -0700

    Fixes import

--- generank/compute/tasks/util.py
+++ generank/compute/tasks/util.py
@@ -1,12 +1,12 @@
-from celery import shared_task, group
+from celery import shared_task, group, subtask
 from django.conf import settings
 
 
 @shared_task
 def dmap(it, callback):
     """ Distributed Map function.
-    Given an iterable of tasks and a callback, perform all
-    of the tasks and wait before calling the callback.
+    Given an iterable of data and a task method, map the method over the
+    given data.
     """
     callback = subtask(callback)
     return group(callback.clone([arg,]) for arg in it)()
