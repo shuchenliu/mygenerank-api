38cbdefb24d4b5182f68aadc5cb347cf76f643d4
Brian Schrader <brian@brianschrader.com>
2017-06-22 15:38:37 -0700

    Adds new template for entire site.
    - Moves site urls to website app.
    - Alters styling on newsfeed to fit template.
    - Adds new about, team, and contact page.

--- generank/website/views/__init__.py
+++ generank/website/views/__init__.py
@@ -1,1 +1,32 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import TemplateHTMLRenderer
+
+from generank.api import models
 from . import newsfeed
+
+
+@api_view(['GET'])
+@renderer_classes((TemplateHTMLRenderer,))
+def home_view(request):
+    users_count = models.User.objects.all().count()
+    return Response({
+        'title': 'MyGeneRank | Unlock your genetic risk',
+        'users': users_count
+    }, template_name='home.html')
+
+
+@api_view(['GET'])
+@renderer_classes((TemplateHTMLRenderer,))
+def team_view(request):
+    return Response({
+        'title': 'MyGeneRank | The Team'
+    }, template_name='team.html')
+
+
+@api_view(['GET'])
+@renderer_classes((TemplateHTMLRenderer,))
+def contact_view(request):
+    return Response({
+        'title': 'MyGeneRank | Contact Us'
+    }, template_name='contact.html')
