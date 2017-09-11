c46995411bb4351b84148f137267ba22cd08bf5b
Brian Schrader <brian@brianschrader.com>
2017-09-11 13:50:04 -0700

    Updates some tests, and removes some old ones.

--- generank/twentythreeandme/tests.py
+++ generank/twentythreeandme/tests.py
@@ -1,12 +1,12 @@
+import json, sure, uuid
+
+import requests
 from unittest.mock import MagicMock
 from unittest.mock import patch
 
-import sure
-import json
-
 from . import api_client
 from .models import Profile, Genotype
-from .tasks import *
+from . import tasks
 from generank.api.models import User as ApiUser
 
 class mock_response(object):
@@ -23,6 +23,7 @@ def test_get():
     response = api_client.get(url,token)
 
     response['test'].should.equal('test123')
+
 
 def test_user_import():
     """ Tests the get_user_info() function in the api_client. """
@@ -55,59 +56,71 @@ def test_genotype_import():
     response['id'].should.equal("SP1_FATHER_V4")
     response['genome'].should.equal("__AAAAAAAAAAAAAA__AA__GGAA")
 
-@patch('generank.twentythreeandme.tasks.get_user_info')
-def test_twentythreeandme_delayed_import_task(get_user_info):
-    """ Tests the twentythreeandme_delayed_import_task function from tasks. """
 
-    token = '8c6fdf907e59f829e0da298d3e4a39be'
-    api_userid = ApiUser().id
-    profileid = "SP1_MOTHER_V4"
-    udata = '{"id": "1a91d9493388f6ed", "profiles": [{"genotyped": true, "id": \
-    "SP1_MOTHER_V4"}, {"genotyped": true, "id": "SP1_FATHER_V4"}], "email": \
-    "bschrade@scripps.edu"}'
-
-    get_user_info.return_value = udata
-    twentythreeandme_import_task.delay = MagicMock(return_value = None)
-
-    twentythreeandme_delayed_import_task(token,api_userid,profileid)
-    twentythreeandme_import_task.delay.assert_called_with(udata,token,
-                                                           api_userid,profileid)
-
-def test_twentythreeandme_import_task():
+# @patch('generank.twentythreeandme.tasks.get_user_info')
+# def test_twentythreeandme_delayed_import_task(get_user_info):
+#     """ Tests the twentythreeandme_delayed_import_task function from tasks. """
+#
+#     token = '8c6fdf907e59f829e0da298d3e4a39be'
+#     api_userid = uuid.uuid4()
+#     profileid = "SP1_MOTHER_V4"
+#     udata = '{"id": "1a91d9493388f6ed", "profiles": [{"genotyped": true, "id": \
+#     "SP1_MOTHER_V4"}, {"genotyped": true, "id": "SP1_FATHER_V4"}], "email": \
+#     "bschrade@scripps.edu"}'
+#
+#     tasks.get_user_info.return_value = udata
+#     tasks.import_account.delay = MagicMock(return_value = None)
+#
+#     tasks.twentythreeandme_delayed_import_task(token,api_userid,profileid)
+#     tasks.twentythreeandme_import_task.delay.assert_called_with(udata,token,
+#                                                            api_userid,profileid)
+#
+def test_import_account():
     """ Tests the twentythreeandme_import_task function from tasks. """
 
-    token = '8c6fdf907e59f829e0da298d3e4a39be'
-    api_userid = ApiUser().id
-    profileid = "SP1_MOTHER_V4"
-    udata = json.loads('{"id": "1a91d9493388f6ed", "profiles": [{"genotyped": \
-    true, "id": "SP1_MOTHER_V4"}, {"genotyped": true, "id": "SP1_FATHER_V4"}], \
-    "email": "bschrade@scripps.edu"}')
+    token = {
+        'access_token': '8c6fdf907e59f829e0da298d3e4a39be',
+        'expires_in': 3600
+    }
+    api_userid = uuid.uuid4()
+    profileid = uuid.uuid4()
 
-    twentythreeandme_genotype_import_task.delay = MagicMock(return_value = None)
-    twentythreeandme_import_task(udata,token,api_userid,profileid)
-    twentythreeandme_genotype_import_task.delay.called.should.equal(True)
+    tasks._import_user.s = MagicMock()
+    tasks._import_profile.s = MagicMock()
+    tasks._import_genotype.si = MagicMock()
 
-@patch('generank.twentythreeandme.tasks.get_genotype_data')
-def test_twentythreeandme_genotype_import_task(get_genotype_data):
-    """ Tests the twentythreeandme_genotype_import_task function from tasks. """
+    tasks.import_account(token, api_userid, profileid)
 
-    gdata = '{"id": "SP1_FATHER_V4", "genome": "__AAAAAAAAAAAAAA__AA__GGAA"}'
-    get_genotype_data.return_value = json.loads(gdata)
-    token = '8c6fdf907e59f829e0da298d3e4a39be'
+    tasks._import_user.s.called.should.equal(True)
+    tasks._import_profile.s.called.should.equal(True)
+    tasks._import_genotype.si.called.should.equal(True)
 
-    convert_genotype_task.delay = MagicMock(return_value=None)
 
-    twentythreeandme_genotype_import_task(Profile(),token)
-    convert_genotype_task.delay.called.should.equal(True)
-
+# @patch('generank.twentythreeandme.tasks.get_genotype_data')
+# def test_twentythreeandme_genotype_import_task(get_genotype_data):
+#     """ Tests the twentythreeandme_genotype_import_task function from tasks. """
+#
+#     gdata = '{"id": "SP1_FATHER_V4", "genome": "__AAAAAAAAAAAAAA__AA__GGAA"}'
+#     get_genotype_data.return_value = json.loads(gdata)
+#     token = '8c6fdf907e59f829e0da298d3e4a39be'
+#
+#     tasks._convert_genotype.delay = MagicMock(return_value=None)
+#     Profile.objects.get = MagicMock(return_value=Profile())
+#
+#     tasks._import_genotype(token, uuid.uuid4(), '')
+#     tasks._convert_genotype.delay.called.should.equal(True)
+#
+#
 @patch('generank.twentythreeandme.tasks.convert')
 def test_convert_genotype_task(convert):
     """ Tests the convert_genotype_task function from tasks. """
 
     gdata = json.loads('{"id": "SP1_FATHER_V4",  \
                             "genome": "__AAAAAAAAAAAAAA__AA__GGAA"}')
-    genotype = Genotype.from_json(gdata,Profile())
+    genotype = Genotype.from_json(gdata, Profile.objects.create(genotyped=True))
+    genotype.save()
     convert.return_value = 'test'
 
-    convert_genotype_task(genotype)
+    tasks._convert_genotype(genotype.id.hex)
+
     convert.assert_called_with("__AAAAAAAAAAAAAA__AA__GGAA")
